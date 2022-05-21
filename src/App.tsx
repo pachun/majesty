e74f038209799e49ffb52c413c239ecd6fe182b6
Nick Pachulski <nick@pachulski.me>
2022-05-21 17:23:25 -0400

    Create nobles

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,154 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import ReactJson from "react-json-view"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface IsabellaOfCastille {
+  name: "Isabella of Castille"
+  requiredDiamondBonuses: 4
+  requiredOnyxBonuses: 4
+}
+const isabellaOfCastille: IsabellaOfCastille = {
+  name: "Isabella of Castille",
+  requiredDiamondBonuses: 4,
+  requiredOnyxBonuses: 4,
 }
 
-export default App;
+interface FrancoisTheFirst {
+  name: "François 1st"
+  requiredOnyxBonuses: 3
+  requiredRubyBonuses: 3
+  requiredEmeraldBonuses: 3
+}
+const francoisTheFirst: FrancoisTheFirst = {
+  name: "François 1st",
+  requiredOnyxBonuses: 3,
+  requiredRubyBonuses: 3,
+  requiredEmeraldBonuses: 3,
+}
+
+interface MaryStuart {
+  name: "Mary Stuart"
+  requiredOnyxBonuses: 3
+  requiredSapphireBonuses: 3
+  requiredDiamondBonuses: 3
+}
+const maryStuart: MaryStuart = {
+  name: "Mary Stuart",
+  requiredOnyxBonuses: 3,
+  requiredSapphireBonuses: 3,
+  requiredDiamondBonuses: 3,
+}
+
+interface CharlesTheFifth {
+  name: "Charles V"
+  requiredOnyxBonuses: 3
+  requiredRubyBonuses: 3
+  requiredDiamondBonuses: 3
+}
+const charlesTheFifth: CharlesTheFifth = {
+  name: "Charles V",
+  requiredOnyxBonuses: 3,
+  requiredRubyBonuses: 3,
+  requiredDiamondBonuses: 3,
+}
+
+interface Machiavelli {
+  name: "Machiavelli"
+  requiredSapphireBonuses: 4
+  requiredDiamondBonuses: 4
+}
+const machiavelli: Machiavelli = {
+  name: "Machiavelli",
+  requiredSapphireBonuses: 4,
+  requiredDiamondBonuses: 4,
+}
+
+interface CatherineOfMedici {
+  name: "Catherine of Medici"
+  requiredEmeraldBonuses: 3
+  requiredSapphireBonuses: 3
+  requiredRubyBonuses: 3
+}
+const catherineOfMedici: CatherineOfMedici = {
+  name: "Catherine of Medici",
+  requiredEmeraldBonuses: 3,
+  requiredSapphireBonuses: 3,
+  requiredRubyBonuses: 3,
+}
+
+interface HenryTheEighth {
+  name: "Henry VIII"
+  requiredOnyxBonuses: 4
+  requiredRubyBonuses: 4
+}
+const henryTheEighth: HenryTheEighth = {
+  name: "Henry VIII",
+  requiredOnyxBonuses: 4,
+  requiredRubyBonuses: 4,
+}
+
+interface AnneOfBrittany {
+  name: "Anne of Brittany"
+  requiredEmeraldBonuses: 3
+  requiredSapphireBonuses: 3
+  requiredDiamondBonuses: 3
+}
+const anneOfBrittany: AnneOfBrittany = {
+  name: "Anne of Brittany",
+  requiredEmeraldBonuses: 3,
+  requiredSapphireBonuses: 3,
+  requiredDiamondBonuses: 3,
+}
+
+interface SolimanTheMagnificent {
+  name: "Soliman the Magnificent"
+  requiredSapphireBonuses: 4
+  requiredEmeraldBonuses: 4
+}
+const solimanTheMagnificent: SolimanTheMagnificent = {
+  name: "Soliman the Magnificent",
+  requiredSapphireBonuses: 4,
+  requiredEmeraldBonuses: 4,
+}
+
+interface ElizabethTheFirstOfEngland {
+  name: "Elizabeth I of England"
+  requiredRubyBonuses: 4
+  requiredEmeraldBonuses: 4
+}
+const elizabethTheFirstOfEngland: ElizabethTheFirstOfEngland = {
+  name: "Elizabeth I of England",
+  requiredRubyBonuses: 4,
+  requiredEmeraldBonuses: 4,
+}
+
+type Noble =
+  | IsabellaOfCastille
+  | FrancoisTheFirst
+  | MaryStuart
+  | CharlesTheFifth
+  | Machiavelli
+  | CatherineOfMedici
+  | HenryTheEighth
+  | AnneOfBrittany
+  | SolimanTheMagnificent
+  | ElizabethTheFirstOfEngland
+
+export const nobles = new Set<Noble>([
+  isabellaOfCastille,
+  francoisTheFirst,
+  maryStuart,
+  charlesTheFifth,
+  machiavelli,
+  catherineOfMedici,
+  henryTheEighth,
+  anneOfBrittany,
+  solimanTheMagnificent,
+  elizabethTheFirstOfEngland,
+])
+export const noblePrestigePoints = 3
+
+function App() {
+  return <ReactJson src={Array.from(nobles)} />
+}
+
+export default App
